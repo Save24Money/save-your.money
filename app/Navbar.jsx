06d8d55dee614592f3b2211902d67c15db34f4b2
lil-aleks <olekiwan4@gmail.com>
2023-10-18 18:29:48 +0200

    Upload all files

---
+++ app/Navbar.jsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import transparentLogo from '../public/logo_transparent.png';
+
+export default function Navbar() {
+
+    return (
+        <div className="sticky top-0 w-screen h-12 bg-secundary-900 md:px-2 px-8 select-none">
+            <input type="checkbox" id="navbar-check" className="hidden peer" />
+            <label htmlFor="navbar-check" className="">
+                <svg className="hidden float-right h-6 my-3 mx-3 cursor-pointer fill-primary-900 md:inline-block" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" /></svg>
+            </label>
+            <Image src={transparentLogo} width={130} height={130} className="hidden  md:inline-block" />
+            <h1 className="inline-block text-xl py-2 font-medium md:hidden">save your money</h1>
+            <ul className="transition-all float-right list-none md:fixed md:w-screen md:float-none md:h-12 md:bg-secundary-500 md:top-[-50px] md:left-0 md:text-center peer-checked:md:top-[48px]">
+                <li className="inline-block py-2 mx-2">
+                    <a href="#" className="leading-8 bg-secundary-300 rounded px-1">Kurse</a>
+                </li>
+                <li className="inline-block py-2 mx-2">
+                    <a href="#" className="leading-8 bg-secundary-300 rounded px-1">Bücher</a>
+                </li>
+            </ul>
+        </div>
+    )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,46 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+    colors: {
+      secundary : {
+        900: '#72dfd0',
+        800: '#7be0d3',
+        700: '#7fdbcf',
+        600: '#86dbd0',
+        500: '#8dd9cf',
+        400: '#93d9d0',
+        300: '#9fccc6',
+        200: '#a7ccc7',
+        100: '#bfdeda',
       },
+      primary : {
+        900: '#03414d',
+        800: '#0a444f',
+        700: '#0d4954',
+        600: '#11505c',
+        500: '#145561',
+        400: '#1a5e6b',
+        300: '#216875',
+        200: '#276f7d',
+        100: '#317d8c',
+      }
     },
+    screens: {
+      '2xl': {'max': '1535px'},
+      // => @media (max-width: 1535px) { ... }
+
+      'xl': {'max': '1279px'},
+      // => @media (max-width: 1279px) { ... }
+
+      'lg': {'max': '1023px'},
+      // => @media (max-width: 1023px) { ... }
+
+      'md': {'max': '767px'},
+      // => @media (max-width: 767px) { ... }
+
+      'sm': {'max': '639px'},
+      // => @media (max-width: 639px) { ... }
+    }
   },
   plugins: [],
 }
